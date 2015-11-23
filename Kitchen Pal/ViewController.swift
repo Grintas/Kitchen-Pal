57a4460caead09c6b5cec2de1f0ec253d3454696
Grintas Junevičius <grju1148@obuolys14.mif>
2015-11-23 16:15:17 +0200

    Initial Commit

---
+++ Kitchen Pal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kitchen Pal
+//
+//  Created by Grintas Junevičius on 11/23/15.
+//  Copyright © 2015 Grintas Junevičius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
